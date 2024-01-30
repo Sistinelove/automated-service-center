ef941fb0d6a1201f56f5f4543573ff53890d0573
Sistinelove <142403330+Sistinelove@users.noreply.github.com>
2024-01-30 15:56:27 +0500

    add prism

--- app/Accounting/page.tsx
+++ app/Accounting/page.tsx
@@ -1,11 +1,15 @@
-'use client'
 import React from 'react';
 import { Input } from '@mui/material';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import db from '@/lib/db';
+import { User } from '@prisma/client';
 
-export default function Accounting() {
+
+export default async function Accounting() {
+    const user = await db.user.findMany();
+    console.log(user);
     return (
-        <div className="flex-row ">
+        <div className="flex-row">
             <div className="flex justify-start items-center m-2">
                 <div className='flex justify-between w-28 mr-8'>
                     <div>
@@ -15,24 +19,16 @@ export default function Accounting() {
                 </div>
                 <Input placeholder='Search client' className='p-1 text-black rounded-sm' />
             </div>
-            <div className='flex '>
-                <div className="flex-col grid-cols-5 p-4 justify-between h-44 w-44 bg-lime-300 rounded-xl mr-4">
-                    <div className='text-center'>name</div>
-                    <div className='text-center'>phone</div>
-                    <div className='text-center'>email</div>
-                </div>
-                <div className="flex-col grid-cols-5 p-4 justify-between h-44 w-44 bg-lime-300 rounded-xl">
-                    <div className='text-center'>name</div>
-                    <div className='text-center'>phone</div>
-                    <div className='text-center'>email</div>
-                </div>
-                <div className="flex-col grid-cols-5 p-4 justify-between h-44 w-44 bg-lime-300 rounded-xl">
-                    <div className='text-center'>name</div>
-                    <div className='text-center'>phone</div>
-                    <div className='text-center'>email</div>
-                </div>
+            <div className='flex'>
+                {user.map((user, index) => (
+                    <div key={index} className="flex-col grid-cols-5 p-4 justify-between h-44 w-44 bg-lime-300 rounded-xl mr-4">
+                        <div className='text-center'>{user.name}</div>
+                        <div className='text-center'>phone</div>
+                        <div className='text-center'>{user.email}</div>
+                    </div>
+                ))}
             </div>
-
         </div>
-    )
+    );
 }
+
